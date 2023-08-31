794f7438a5daa4927d01c3e585ec3a7f1f4c480a
Drine Kin <foolishgirl152178@gmail.com>
2023-08-31 23:52:09 +0630

    wavesurfer js current listening margin fixed

--- src/components/CurrentListening.jsx
+++ src/components/CurrentListening.jsx
@@ -4,7 +4,7 @@ import Waveform from "./WaveForm";
 
 const CurrentListening = (inputText) => {
 	return (
-		<section className="-ml-5 -mr-5 fixed left-0 right-0 lg:sticky bottom-0 ">
+		<section className="m-auto lg:-ml-5 lg:-mr-5 fixed left-0 right-0 lg:sticky bottom-0 ">
 			<div className="">
 				<div className="flex flex-col  bg-cardColor p-4">
 					{/* <div className="flex flex-row justify-center items-center "> */}
